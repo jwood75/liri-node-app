bfbec9acfcbcb2a5bf0b9567a223486cb00ff22f
joshua wood <joshua wood>
2017-04-24 14:24:50 -0400

    created more essential files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+
+  consumer_key: 'HWH7BVzNzRsCDoZGrMQ9SPq91',
+  consumer_secret: '3u3dDpC77hUL9cOVB9ZDfnri986W7lo4wmZhzV3g5wIVIUq4Pi',
+  access_token_key: '247081749-8I5KERnqbKlnGjZ307USWinadioWeMmRv7Lag5GE',
+  access_token_secret: 'M1iZnUJzAvFBUPWcUdoD5jTyIgQUI8WJUagVLcqYvI3xF',
+
+}
